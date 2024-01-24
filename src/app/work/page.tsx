86ccaca88a5453185511fd70b0711ced8584b1e7
natsuki <yasashisainternet@gmail.com>
2024-01-25 07:17:13 +0900

    [add] /app/work/page.tsxを追加
    /workで表示するページを作成しました。

---
+++ src/app/work/page.tsx
@@ -0,0 +1,39 @@
+import { client } from '@/lib/client';
+import { RootObject } from '@/types/index.type';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default async function Work() {
+	const data = await client.get<RootObject>({ endpoint: 'work' });
+
+	return (
+		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12'>
+			{data.contents.map((content, index) => (
+				<div
+					key={index}
+					className='card bg-base-200'
+				>
+					<figure>
+						<Image
+							src={content.thumbnail.url} // thumbnailのurlを使用
+							alt={content.title}
+							width={400}
+							height={400}
+						/>
+					</figure>
+					<div className='card-body'>
+						<h2 className='card-title'>{content.title}</h2>
+						<div className='card-actions'>
+							<Link
+								className='btn btn-primary'
+								href={'/work/' + content.id}
+							>
+								詳細を見る
+							</Link>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
