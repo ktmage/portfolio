f7298f5ed81338b6591a66f7e84fad4055ecefd4
natsuki <yasashisainternet@gmail.com>
2024-03-02 01:13:21 +0900

    [update] : ヘッダーのデザインを修正

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -9,24 +9,30 @@ export default function Header() {
 	];
 
 	return (
-		<header className='navbar bg-base-200 sticky top-0 z-50'>
-			<div className='flex-1'>
+		<header className='navbar min-h-0 h-16 p-0 bg-base-200 sticky top-0 z-50'>
+			<div className='flex-1 h-full'>
 				<Link
 					href={'/'}
-					className='text-xl btn btn-ghost'
+					className='h-full text-xl btn btn-ghost rounded-none no-animation'
 				>
 					Portfolio
 				</Link>
 			</div>
-			<div className='flex-none'>
-				<ul className='menu menu-horizontal px-1 space-x-4'>
-					{menu.map((item, index) => (
-						<li key={index}>
-							<Link href={item.href}>{item.name}</Link>
-						</li>
-					))}
-				</ul>
-			</div>
+			<ul className='h-full'>
+				{menu.map((item, index) => (
+					<li
+						key={index}
+						className='h-full'
+					>
+						<Link
+							href={item.href}
+							className='h-full w-24 btn btn-ghost rounded-none no-animation'
+						>
+							{item.name}
+						</Link>
+					</li>
+				))}
+			</ul>
 		</header>
 	);
 }
