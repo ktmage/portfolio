a788476a0d918a712ba67f675a95181124f07f66
natsuki <yasashisainternet@gmail.com>
2024-03-03 23:45:26 +0900

    [update] : footerにz-indexを追加

--- src/components/ui/footer/index.tsx
+++ src/components/ui/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className='footer footer-center p-6 bg-base-200'>
+		<footer className='footer footer-center p-6 bg-base-200 z-50'>
 			<p>Copyright © 2024 - ktmage</p>
 		</footer>
 	);
