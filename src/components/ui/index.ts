27e3b377aa5fa06b409d58d4c27ba7d6b28cb71a
natsuki <yasashisainternet@gmail.com>
2024-03-07 09:52:11 +0900

    [fix] : フォルダの名前を変更

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,2 @@
-export { default as Header } from './header';
-export { default as Footer } from './footer';
-export { default as Background } from './background';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
