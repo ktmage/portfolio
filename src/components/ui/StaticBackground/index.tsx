b63121402d85325be67781d25ae3ff6f76f5cbd4
natsuki <yasashisainternet@gmail.com>
2024-03-10 07:19:40 +0900

    [update] : 背景を固定

--- src/components/ui/StaticBackground/index.tsx
+++ src/components/ui/StaticBackground/index.tsx
@@ -15,14 +15,15 @@ export default function StaticBackground(props: StaticBackgroundProps) {
 	const { image, isActiveBlur } = props;
 
 	return (
-		<Image
-			src={image.src}
-			alt='background'
-			fill={true}
-			objectFit='cover'
-			quality={100}
-			priority={true}
-			className={`transition duration-75 ${isActiveBlur ? 'blur-2xl' : 'blur-none'}`}
-		/>
+		<div className='fixed top-0 left-0 w-full h-full'>
+			<Image
+				src={image.src}
+				alt='background'
+				fill
+				quality={100}
+				priority={true}
+				className={`object-cover transition duration-75 ${isActiveBlur ? 'blur-2xl' : 'blur-none'}`}
+			/>
+		</div>
 	);
 }
