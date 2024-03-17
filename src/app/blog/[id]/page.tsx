c303c079b366598bf21ffe34921b0370f59c2f53
natsuki <yasashisainternet@gmail.com>
2024-03-17 21:18:23 +0900

    [update] : 文章が見やすいように背景を追加

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { client } from '@/lib/client';
 import { BlogContent } from '@/types/index.type';
-import Image from 'next/image';
 import parse from 'html-react-parser';
 
 export default async function Detail({ params }: { params: { id: string } }) {
@@ -8,10 +7,12 @@ export default async function Detail({ params }: { params: { id: string } }) {
 	const date = new Date(data.updatedAt).toLocaleString();
 
 	return (
-		<div className='flex flex-col items-center justify-center w-full'>
-			<h1 className='text-4xl font-bold mb-2'>{data.title}</h1> {/* タイトルを表示 */}
-			<p className='text-gray-600 mb-4'>Updated at: {date}</p> {/* 更新日時を表示 */}
-			<div className='prose max-w-3xl'>{parse(data.content)}</div>
+		<div className='flex flex-row justify-center my-4 bg-base-200 drop-shadow-2xl'>
+			<div className='flex flex-col items-center justify-center   py-24 px-36 bg-base-100'>
+				<h1 className='text-4xl font-bold mb-2'>{data.title}</h1> {/* タイトルを表示 */}
+				<p className='text-gray-600 mb-4'>Updated at: {date}</p> {/* 更新日時を表示 */}
+				<div className='prose max-w-3xl'>{parse(data.content)}</div>
+			</div>
 		</div>
 	);
 }
