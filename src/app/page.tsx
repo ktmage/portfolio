26b0e982c26332a85b36b6a8145fe534e57ce87f
natsuki <yasashisainternet@gmail.com>
2024-03-18 21:38:11 +0900

    [update] : app/page.tsxを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function RootPage() {
-	return <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>root page</div>;
+	return <div></div>;
 }
